2269a5d02914aad7fbd0a7ba18b3699c04cb7005
Lucas Weiblen <lucasweiblen@gmail.com>
2014-07-25 16:16:44 -0300

    Add stack

---
+++ stack.go
@@ -0,0 +1,64 @@
+package main
+
+import "fmt"
+import "strconv"
+
+type Stack struct {
+  structure []interface{}
+}
+
+func (s *Stack) push(value interface{}) {
+  s.structure = append(s.structure, value)
+}
+
+func (s *Stack) pop() interface{} {
+  s.structure = s.structure[:s.length()-1]
+  return s.structure[s.length()-1]
+}
+
+func (s *Stack) length() int {
+  return len(s.structure)
+}
+
+func NewStack() *Stack {
+  stack := &Stack{}
+  return stack
+}
+
+func (s *Stack) String() string {
+  var a string
+  for _, v := range s.structure {
+    if value, ok := v.(string); ok {
+      a += value + "\n"
+    }
+    if number, ok := v.(int); ok {
+      a += strconv.Itoa(number) + "\n"
+    }
+  }
+  return a
+}
+
+func (s *Stack) print() {
+  for _, v := range s.structure {
+    fmt.Println(v)
+  }
+}
+
+func main() {
+  stringStack := NewStack()
+  stringStack.push("bla")
+  stringStack.push("teste")
+
+  fmt.Println(stringStack)
+
+  intStack := NewStack()
+  intStack.push(1)
+  intStack.push(3)
+  intStack.push(5)
+
+  fmt.Println(intStack)
+
+  intStack.pop()
+
+  fmt.Println(intStack)
+}
