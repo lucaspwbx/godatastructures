6518639d12d8b1e90cb21974c4b3ea12b2432cfa
Lucas Weiblen <lucasweiblen@gmail.com>
2014-07-27 20:33:20 -0300

    Refactoring

--- stack.go
+++ stack.go
@@ -1,57 +1,38 @@
-package main
+package stack
 
-import "fmt"
 import "strconv"
 
 type Stack struct {
-  structure []interface{}
+	structure []interface{}
 }
 
 func (s *Stack) push(value interface{}) {
-  s.structure = append(s.structure, value)
+	s.structure = append(s.structure, value)
 }
 
 func (s *Stack) pop() interface{} {
-  s.structure = s.structure[:s.length()-1]
-  return s.structure[s.length()-1]
+	value := s.structure[s.length()-1]
+	s.structure = s.structure[:s.length()-1]
+	return value
 }
 
 func (s *Stack) length() int {
-  return len(s.structure)
+	return len(s.structure)
 }
 
 func NewStack() *Stack {
-  return &Stack{}
+	return &Stack{}
 }
 
 func (s *Stack) String() string {
-  var a string
-  for _, v := range s.structure {
-    if value, ok := v.(string); ok {
-      a += value + "\n"
-    }
-    if number, ok := v.(int); ok {
-      a += strconv.Itoa(number) + "\n"
-    }
-  }
-  return a
-}
-
-func main() {
-  stringStack := NewStack()
-  stringStack.push("bla")
-  stringStack.push("teste")
-
-  fmt.Println(stringStack)
-
-  intStack := NewStack()
-  intStack.push(1)
-  intStack.push(3)
-  intStack.push(5)
-
-  fmt.Println(intStack)
-
-  intStack.pop()
-
-  fmt.Println(intStack)
+	var a string
+	for _, v := range s.structure {
+		if value, ok := v.(string); ok {
+			a += value + "\n"
+		}
+		if number, ok := v.(int); ok {
+			a += strconv.Itoa(number) + "\n"
+		}
+	}
+	return a
 }
---
+++ stack_test.go
@@ -0,0 +1,40 @@
+package stack
+
+import "testing"
+
+func TestNewStack(t *testing.T) {
+	stack := NewStack()
+	if stack.structure != nil {
+		t.Errorf("Stack structure should be nil")
+	}
+}
+
+func TestStackLength(t *testing.T) {
+	stack := NewStack()
+	if stack.length() != 0 {
+		t.Errorf("Length should be 0")
+	}
+}
+
+func TestStackPush(t *testing.T) {
+	stack := NewStack()
+	stack.push(1)
+	if stack.length() != 1 {
+		t.Errorf("Stack should have length 1")
+	}
+	if stack.structure[0] != 1 {
+		t.Errorf("Stack item should be 1")
+	}
+}
+
+func TestStackPop(t *testing.T) {
+	stack := NewStack()
+	stack.push(2)
+	teste := stack.pop()
+	if stack.length() != 0 {
+		t.Errorf("Stack should have length 0")
+	}
+	if teste != 2 {
+		t.Errorf("Item popped should be 2")
+	}
+}
