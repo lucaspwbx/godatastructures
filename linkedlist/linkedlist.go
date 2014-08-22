e5de6d441305711da111f3c4c8e43a979d3dcd85
Lucas Weiblen <lucasweiblen@gmail.com>
2014-08-22 00:04:49 -0300

    Add linked list

---
+++ linkedlist/linkedlist.go
@@ -0,0 +1,52 @@
+package linkedlist
+
+type Node struct {
+	element interface{}
+	next    *Node
+}
+
+type LinkedList struct {
+	head *Node
+}
+
+func (l *LinkedList) Find(item interface{}) *Node {
+	currNode := l.head
+	for {
+		if currNode.element != item {
+			currNode = currNode.next
+		}
+	}
+	return currNode
+}
+
+func (l *LinkedList) Insert(newElement interface{}, item interface{}) {
+	newNode := NewNode(newElement)
+	current := l.Find(item)
+	newNode.next = current.next
+	current.next = newNode
+}
+
+func (l *LinkedList) FindPrevious(item interface{}) *Node {
+	currNode := l.head
+	for {
+		if currNode.next != nil && currNode.next.element != item {
+			currNode = currNode.next
+		}
+	}
+	return currNode
+}
+
+func (l *LinkedList) Remove(item interface{}) {
+	prevNode := l.FindPrevious(item)
+	if prevNode.next != nil {
+		prevNode.next = prevNode.next.next
+	}
+}
+
+func NewNode(element interface{}) *Node {
+	return &Node{element, nil}
+}
+
+func NewLinkedList() *LinkedList {
+	return &LinkedList{head: NewNode("head")}
+}
